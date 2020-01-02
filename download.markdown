74f18064c12e082d8e56eb27182817453a9dbcc0
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2020-01-02 09:45:53 -0500

    Reverse engineeer releases

---
+++ download.markdown
@@ -0,0 +1,15 @@
+---
+title: Download ScalaCheck
+---
+
+# Download ScalaCheck
+
+ScalaCheck build artifacts are no longer published on this site. You can find builds published in [The Central Repository](https://search.maven.org/).
+
+The easiest way to retrieve ScalaCheck is through sbt:
+
+```scala
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.14.1" % "test"
+```
+
+There are still [older releases](releases.html) available on this site.
--- site.hs
+++ site.hs
@@ -1,6 +1,7 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
+import           Data.Maybe  (fromMaybe)
 import           Hakyll
 
 
@@ -27,9 +28,28 @@ main = hakyll $ do
 
     match "download/*.markdown" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/download.html" downloadCtx
-            >>= relativizeUrls
+        compile $ do
+            let releaseCtx =
+                    constField "foo" "bar" `mappend`
+                    siteCtx
+                downloadCtx =
+                    -- This defines a field "scalaVersions" in the
+                    -- YAML preamble that is a list and can be
+                    -- iterated over in the template file.  However,
+                    -- this does not support either a single value nor
+                    -- a comma-separated list of values.
+                    listFieldWith "scalaVersions"
+                        (field "scalaver" (return . itemBody) `mappend`
+                         releaseCtx)
+                        (\item -> do
+                          metadata <- getMetadata (itemIdentifier item)
+                          let scalaVersions = fromMaybe [] $ lookupStringList "scalaVersions" metadata
+                          mapM makeItem scalaVersions) `mappend`
+                          releaseCtx
+            pandocCompiler
+                >>= loadAndApplyTemplate "templates/download.html" downloadCtx
+                >>= loadAndApplyTemplate "templates/default.html"  siteCtx
+                >>= relativizeUrls
 
     match "index.markdown" $ do
         route   $ setExtension "html"
@@ -50,6 +70,19 @@ main = hakyll $ do
             pandocCompiler
                 >>= loadAndApplyTemplate "templates/default.html" indexCtx
 
+    create ["releases.html"] $ do
+        route idRoute
+        compile $ do
+            releases <- recentFirst =<< loadAll "download/*.markdown"
+            let releasesCtx =
+                    listField "releases" siteCtx (return releases) `mappend`
+                    constField "title" "All ScalaCheck Releases"   `mappend`
+                    siteCtx
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/releases.html" releasesCtx
+                >>= loadAndApplyTemplate "templates/default.html"  siteCtx
+                >>= relativizeUrls
+
     match "templates/*" $ compile templateBodyCompiler
 
 
@@ -58,8 +91,3 @@ siteCtx :: Context String
 siteCtx =
     dateField "year" "%Y" `mappend`
     defaultContext
-downloadCtx :: Context String
-downloadCtx =
-    constField "currentVer" "1.14.0" `mappend`
-    dateField "date" "%B %e, %Y" `mappend`
-    siteCtx
